1473c968a05b36be6d134b3336a0f43bfea805c8
Natalie Lampa <nlampa@umich.edu>
2020-12-09 12:22:32 -0500

    Initial Commit

---
+++ beatstreet1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beatstreet1.0
+//
+//  Created by Natalie Lampa on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ beatstreet1.0Tests/beatstreet1_0Tests.swift
@@ -0,0 +1,33 @@
+//
+//  beatstreet1_0Tests.swift
+//  beatstreet1.0Tests
+//
+//  Created by Natalie Lampa on 12/9/20.
+//
+
+import XCTest
+@testable import beatstreet1_0
+
+class beatstreet1_0Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
